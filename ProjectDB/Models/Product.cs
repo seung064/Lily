53a0f7b162d7d4d9315451ff3e248bf1b799a473
seung064 <gli056@naver.com>
2025-05-25 21:28:10 +0900

    1

---
+++ ProjectDB/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDB.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductNum { get; set; }
+
+        [Required, MaxLength(100)]
+        public string ProductName { get; set; }
+
+        public string Description_P { get; set; }
+
+        public string Receipt { get; set; }
+
+    }
+}
