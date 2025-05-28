8577cd4735334e95048124d55bfcf6ea3fa711e8
r2d2c2 <uy6806@naver.com>
2025-05-28 13:15:15 +0900

    socket start

--- Project_Lily/MainWindow.xaml.cs
+++ Project_Lily/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Net.Sockets;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,5 +21,27 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    async Task Server()
+    {
+        TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 51111);
+        listener.Start();
+        using (TcpClient c = listener.AcceptTcpClient())
+        {
+            using (NetworkStream stream = c.GetStream())
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                MessageBox.Show("Received: " + message, "Server Message", MessageBoxButton.OK, MessageBoxImage.Information);
+                // 안녕 클라이언트
+                BinaryWriter writer = new BinaryWriter(stream);
+                writer.Write("hello 클라이언트");
+                // 종료
+                stream.Close();
+            }
+            listener.Stop();
+        }
     }
 }
