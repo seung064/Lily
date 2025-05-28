1b671e643db4db499e8860405639bda728bca01d
seung064 <gli056@naver.com>
2025-05-28 09:36:09 +0900

    1

--- Project_Lily/obj/Debug/net8.0-windows/Project_Lily.AssemblyInfo.cs
+++ Project_Lily/obj/Debug/net8.0-windows/Project_Lily.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project_Lily")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cab8eeb37ad91e5f6f3c3615f2107f0513e597d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project_Lily")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project_Lily")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
